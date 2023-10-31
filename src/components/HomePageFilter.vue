f03d4f24db1d8dace79939cb763e1b7c94911928
Ростислав Яковец <yakovets.rr@shercargo.ru>
2023-10-31 18:19:31 +0300

    feat:add div homefilterpage

---
+++ src/components/HomePageFilter.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="$style.homePageFiler">
+    <div :class="$style.filter">
+      <VSelect label="Тип недвижимости" />
+      <VBtn>1</VBtn>
+      <VBtn>2</VBtn>
+      <VBtn>3</VBtn>
+      <VBtn>4</VBtn>
+      <div>
+        <label>Стоимость, млн. ₽</label>
+        <div>
+          <input
+            type="text"
+            value="100"
+          />
+          <input
+            type="text"
+            value="200"
+          />
+        </div>
+        <VRangeSlider />
+      </div>
+      <div>
+        <label>Площадь, м2</label>
+        <div>
+          <input
+            type="text"
+            value="1"
+          />
+          <input
+            type="text"
+            value="2"
+          />
+        </div>
+        <VRangeSlider />
+      </div>
+      <vBtn>Показать</vBtn>
+    </div>
+  </div>
+</template>
+
+<script></script>
+<style module>
+.homePageFiler {
+  padding: 12px 12px;
+  margin-top: 3px;
+  border-radius: 20px;
+  border-width: 3px;
+  border-color: black;
+  border-style: solid;
+}
+.filter {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 18px;
+}
+
+.select {
+  width: 330px;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,17 +1,20 @@
 <template>
   <div :class="$style.homePage">
     <HomeSlider />
+    <HomePageFilter />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import HomeSlider from '../components/HomeSlider.vue'
+import HomePageFilter from '../components/HomePageFilter.vue'
 
 export default defineComponent({
   name: 'HomePage',
   components: {
     HomeSlider,
+    HomePageFilter,
   },
 })
 </script>
